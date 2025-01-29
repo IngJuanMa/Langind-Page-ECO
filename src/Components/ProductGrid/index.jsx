1991c0d5ad91686137dc56ac2690c71ebfc7c2b9
Juan Manuel Castro <juanmanuel.castrochavarro@gmail.com>
2025-01-29 11:06:24 -0500

    Corrección errores y imagen de página

--- src/Components/ProductGrid/index.jsx
+++ src/Components/ProductGrid/index.jsx
@@ -4,7 +4,7 @@ import ProductCard from "../ProductsCard";
 const products = [
     { id: 1, name: "Limpiador Multiusos ECO 1L (Lavanda)", price: "6.000", descripcion: "Delicioso aroma a lavanda, ideal para cualquier superficie", image: "/product/limpiador.png" },
     { id: 2, name: "Limpiador Multiusos ECO 5L (Lavanda)", price: "20.000", descripcion: "Delicioso aroma a lavanda, ideal para cualquier superficie", image: "/product/mcincolitro.png" },
-    { id: 3, name: "Jabón Lavaplatos ECO 500mL", price: "7.000", descripcion: "excelente calidad con delicioso aroma a limón", image: "/product/lavaplatos.png" },
+    { id: 3, name: "Jabón Lavaplatos ECO 500mL", price: "5.000", descripcion: "excelente calidad con delicioso aroma a limón", image: "/product/lavaplatos.png" },
     { id: 4, name: "Detergente Liquido ECO 2L", price: "10.000", descripcion: "Delicioso aroma con tecnología de eliminación de olores", image: "/product/deter.png" }
 ];
 
